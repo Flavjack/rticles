1a67f1ee6902571a517a71aad9f4c9ccd984c1cb
Flavjack <flavjack@gmail.com>
2019-11-12 15:06:09 +0100

    include fig & grph index

--- cnfg/setup.r
+++ cnfg/setup.r
@@ -1,27 +1,35 @@
 
-knitr::opts_chunk$set(fig.align = "center",
-	echo = FALSE, message = FALSE, warning = FALSE)
+knitr::opts_chunk$set(
+  fig.align = "center", # Center images in the export file
+	echo = FALSE, # Avoid print code in the export file
+  message = FALSE,  # Avoid print messages in the export file
+  warning = FALSE # Avoid print messages in the export file
+  )
 
-options(digits = 4, OutDec= ".",
-        knitr.kable.NA = '',
-        knitr.table.format = "pandoc", 
-        citr.use_betterbiblatex = FALSE)
+options(
+  digits = 4, # Number of digits in the values
+  OutDec= ".", # Use "." insted of "," in the decimal values
+  scipen = 99, # Avoid use "6e-04"
+  knitr.kable.NA = '', # NA values will appear as empty cell
+  knitr.table.format = "pandoc", # Format for export tables
+  citr.use_betterbiblatex = FALSE # For zotero addin 
+  ) 
 
-library(devtools)
-library(bookdown)
-library(citr)
-library(knitr) 
-library(inserttable)
-library(tidyverse)
-library(googlesheets)
-library(agricolae)
-library(GerminaR)
-library(compareGroups)
-library(FactoMineR)
-library(cowplot)
-library(grid)
-library(png)
-library(jpeg)
+library(devtools) # For install packages and complements from dev sources
+library(bookdown) # Required for use the present template
+library(citr) # Use Zotero for citations
+library(knitr) # Base of the bookdown template
+library(inserttable) # Insert table with copy and paste
+library(tidyverse) # Data manipulation
+library(googlesheets) # Read/write google sheets files
+library(agricolae) # Agriculture data analisys
+library(GerminaR) # Germination analisys and line and bar easy plots
+library(compareGroups) # Tables for compare treatments 
+library(FactoMineR) # Multivariate data anallisys
+library(cowplot) # For export and mergue figures 
+library(grid) # For mergue figures 
+library(png) # Import png files
+library(jpeg) # Import jpeg files
 
 
 
