391a1250fa99ab426c90f6d630b6d3809a28787d
flavjack <flavjack@gmail.com>
2020-03-05 09:30:00 +0100

    add function info

--- cnfg/debug.r
+++ cnfg/debug.r
@@ -1,11 +1,13 @@
 
+# -------------------------------------------------------------------------
+# debug -------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
 if(file.exists("rticles.Rmd"))
-  {file.remove("rticles.Rmd")}
+{file.remove("rticles.Rmd")}
 if(dir.exists("libs"))
-  {fs::dir_delete("libs")}
+{fs::dir_delete("libs")}
 if(dir.exists("docs"))
-  {fs::dir_delete("docs")}
+{fs::dir_delete("docs")}
 if(file.exists("rticles-template-for-articles-using-bookdown-package-.html"))
-  {file.remove("rticles-template-for-articles-using-bookdown-package-.html")}
-
-
+{file.remove("rticles-template-for-articles-using-bookdown-package-.html")}
